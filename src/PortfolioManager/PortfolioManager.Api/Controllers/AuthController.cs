ae228adf92b357af743bff24e96d4acfae7e84b1
Wil <133708710+wilgarxia@users.noreply.github.com>
2024-04-14 17:03:48 -0300

    feat: added jwt token gen

---
+++ src/PortfolioManager/PortfolioManager.Api/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace PortfolioManager.Api.Controllers;
+
+public class AuthController(IConfiguration configuration) : ControllerBase
+{
+    private readonly IConfiguration _configuration = configuration;
+    private static readonly TimeSpan TokenLifetime = TimeSpan.FromHours(1);
+
+    [HttpPost("token")]
+    public IActionResult GenerateToken([FromBody] TokenGenerationRequest request)
+    {
+        JwtSecurityTokenHandler tokenHandler = new();
+        byte[] keyBytes = Encoding.UTF8.GetBytes(_configuration["JwtSettings:Key"]!);
+        List<Claim> claims =
+        [
+            new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new(JwtRegisteredClaimNames.Sub, request.Email),
+            new(JwtRegisteredClaimNames.Email, request.Email),
+            new("userid", request.UserId.ToString()),
+        ];
+
+        foreach (var claimPair in request.CustomClaims)
+        {
+            var jsonElement = (JsonElement)claimPair.Value;
+            var valueType = jsonElement.ValueKind switch
+            {
+                JsonValueKind.True => ClaimValueTypes.Boolean,
+                JsonValueKind.False => ClaimValueTypes.Boolean,
+                JsonValueKind.Number => ClaimValueTypes.Double,
+                _ => ClaimValueTypes.String
+            };
+
+            var claim = new Claim(claimPair.Key, claimPair.Value.ToString()!, valueType);
+            claims.Add(claim);
+        }
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.Add(TokenLifetime),
+            Issuer = _configuration["JwtSettings:Issuer"]!,
+            Audience = _configuration["JwtSettings:Audience"]!,
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(keyBytes), SecurityAlgorithms.HmacSha256)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        var jwt = tokenHandler.WriteToken(token);
+        return Ok(jwt);
+    }
+
+}
+
+public record TokenGenerationRequest(Guid UserId, string Email, Dictionary<string, JsonElement> CustomClaims);
--- src/PortfolioManager/PortfolioManager.Api/Program.cs
+++ src/PortfolioManager/PortfolioManager.Api/Program.cs
@@ -1,13 +1,34 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Text;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x => x.TokenValidationParameters = new TokenValidationParameters
+{
+    ValidIssuer = config["JwtSettings:Issuer"],
+    ValidAudience = config["JwtSettings:Audience"],
+    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtSettings:Key"]!)),
+    ValidateIssuer = true,
+    ValidateAudience = true,
+    ValidateLifetime = true,
+    ValidateIssuerSigningKey = true
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -15,6 +36,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 
 var summaries = new[]
 {
